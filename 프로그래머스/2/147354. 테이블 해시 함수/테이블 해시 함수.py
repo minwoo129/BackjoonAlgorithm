c83adec749b95a714660d6883bef000cfbdfa38e
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-04 12:26:15 +0900

    [level 2] Title: 테이블 해시 함수, Time: 42.65 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -0,0 +1,15 @@
+def solution(data, col, row_begin, row_end):
+    answer = 0
+    data.sort(key = lambda x : (x[col-1], -x[0]))
+    
+    for i in range(row_begin, row_end+1):
+        s = 0
+        
+        for n in data[i-1]:
+            s += (n % i)
+        
+        if answer == 0:
+            answer = s
+        else:
+            answer ^= s
+    return answer
