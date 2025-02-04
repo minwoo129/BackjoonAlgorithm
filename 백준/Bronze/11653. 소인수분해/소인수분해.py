d55c27fcef292988a257be1d12f3e42a333c3437
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-04 12:56:30 +0900

    [Bronze I] Title: 소인수분해, Time: 1136 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+if n == 1:
+    print('')
+
+for i in range(2, n+1):
+    if n % i == 0:
+        while n % i == 0:
+            print(i)
+            n = n / i
