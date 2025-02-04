303aecfd524ee7e1b71a0549a17f161abf092220
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-04 12:53:04 +0900

    [Bronze IV] Title: 숫자의 합, Time: 196 ms, Memory: 14624 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String input = br.readLine();
+		
+		int sum = 0;
+		for(int i = 0; i < input.length(); i++) sum += ((int)input.charAt(i) - 48);
+		System.out.println(sum);
+		br.close();
+	}
+}
