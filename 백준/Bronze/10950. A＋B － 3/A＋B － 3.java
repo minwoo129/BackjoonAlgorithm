a9d6ef9ae631fee769841303c533c64379dc7f3a
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-04 12:47:55 +0900

    [Bronze V] Title: A+B - 3, Time: 272 ms, Memory: 18528 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+ 
+		int[] ansArr = new int[t];
+		for(int i = 0; i < t; i++) {
+			int a = sc.nextInt(), b = sc.nextInt();
+			ansArr[i] = a+b;
+		}
+ 
+		for(int num:ansArr) System.out.println(num);
+	}
+}
