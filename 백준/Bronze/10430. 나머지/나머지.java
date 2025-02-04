ce777992ebdd318624f0a6846bbccc52eb1cbb4f
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-04 12:44:40 +0900

    [Bronze V] Title: 나머지, Time: 220 ms, Memory: 18256 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+ 
+		System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+	}
+}
