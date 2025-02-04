ffde9dbeebf7c8ea956ccda5eb875d5671f3c980
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-04 12:45:57 +0900

    [Bronze V] Title: 개, Time: 136 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+	}
+}
