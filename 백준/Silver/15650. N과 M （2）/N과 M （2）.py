5657d4da206a99537216c9d3bcbfc1e133354a51
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-04 13:22:56 +0900

    [Silver III] Title: N과 M (2), Time: 136 ms, Memory: 35556 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,28 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+numArr = [ i+1 for i in range(n) ]
+results = []
+
+def findValues(a, l):
+
+    if l == m:
+        newArr = a[:]
+        newArr.sort()
+        results.append(tuple(newArr))
+        return
+
+    for num in numArr:
+        newArr = a[:]
+        if num in newArr:
+            continue
+        newArr.append(num)
+        findValues(newArr, l+1)
+
+
+findValues([], 0)
+results = list(set(results))
+results.sort()
+
+for item in results:
+    print(' '.join(map(str, list(item))))
