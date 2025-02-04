ea5268cc9b0705bdad42f8ec9c0f6320f24b87e7
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-04 12:50:02 +0900

    [Bronze V] Title: A+B - 5, Time: 136 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while(true) {
+			String[] input = br.readLine().split(" ");
+			int a = Integer.parseInt(input[0]), b = Integer.parseInt(input[1]);
+			
+			if((a != 0) || (b != 0)) System.out.println((a+b));
+			else break;
+		}
+	}
+}
