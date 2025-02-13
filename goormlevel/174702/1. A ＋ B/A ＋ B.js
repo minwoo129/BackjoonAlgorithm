f75c98cbd9c950a5af6a8fa985383884ac115075
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-13 10:57:56 +0900

    [난이도 1] Title: A + B, Time: 0.03 ms, Memory: 31.48 MB -BaekjoonHub

---
+++ goormlevel/174702/1. A ＋ B/A ＋ B.js
@@ -0,0 +1,18 @@
+// Run by Node.js
+const readline = require('readline');
+
+(async () => {
+	let rl = readline.createInterface({ input: process.stdin });
+	let A = -1, B = -1
+	for await (const line of rl) {
+		const [a, b] = line.split(' ').map(item => item * 1)
+		
+		A = a;
+		B = b
+		rl.close();
+	}
+	
+	console.log(A+B)
+	
+	process.exit();
+})();
