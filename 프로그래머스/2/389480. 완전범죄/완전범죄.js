c0bc766a2a7897eeddd7e27f1d142ecbb513ae19
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2025-02-22 21:57:43 +0900

    [level 2] Title: 완전범죄, Time: 15.39 ms, Memory: 40.2 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.js
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.js
@@ -1,45 +1,4 @@
 function solution(info, n, m) {
-    const len = info.length;
-    let minATrace = Infinity;
-    
-    const cache = new Map()
-    
-    const getCacheKey = (idx, aTrace, bTrace) => `${idx},${aTrace},${bTrace}`
-    
-    // DFS로 모든 가능한 조합을 탐색
-    function dfs(index, aTrace, bTrace) {
-        // 기저 조건: 모든 물건을 확인했을 때
-        if (index === len) {
-            // A와 B 모두 경찰에 붙잡히지 않는 경우에만 고려
-            if (aTrace < n && bTrace < m) {
-                minATrace = Math.min(minATrace, aTrace);
-            }
-            return;
-        }
-        
-        const cacheKey = getCacheKey(index, aTrace, bTrace)
-        if(cache.has(cacheKey)) {
-            return
-        }
-        
-        cache.set(cacheKey, true)
-        
-        // A가 물건을 훔치는 경우
-        const newATrace = aTrace + info[index][0];
-        if (newATrace < n) { // A가 경찰에 붙잡히지 않을 경우에만 진행
-            dfs(index + 1, newATrace, bTrace);
-        }
-        
-        // B가 물건을 훔치는 경우
-        const newBTrace = bTrace + info[index][1];
-        if (newBTrace < m) { // B가 경찰에 붙잡히지 않을 경우에만 진행
-            dfs(index + 1, aTrace, newBTrace);
-        }
-    }
-    
-    // 초기 호출
-    dfs(0, 0, 0);
-    
-    // 결과 반환
-    return minATrace === Infinity ? -1 : minATrace;
+    var answer = 0;
+    return answer;
 }
