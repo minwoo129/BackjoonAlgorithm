76c83a5d02e5372e406df31661e57c586530e71a
minwoo yu <73742422+minwoo129@users.noreply.github.com>
2026-03-05 10:25:56 +0900

    [Bronze II] Title: 분산처리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,35 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    a, b = map(int, sys.stdin.readline().split())
+    
+    a = a % 10
+    
+    if a == 0:
+        print(10)
+        continue
+    if a == 1 or a == 5 or a == 6:
+        print(a)
+        continue
+    
+    if a == 4 or a == 6:
+        b = b % 2
+        
+        if b == 1:
+            print(a)
+        else:
+            print((a*a)%10)
+        continue
+    
+    
+    b = b % 4
+    
+    if b == 0:
+        print((a**4)%10%10%10)
+    else:
+        print((a**b)%10%10%10)
+        
+    
+    
